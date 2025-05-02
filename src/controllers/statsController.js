e8ca3e65cd256cc6967c20f03edf65830c1640a6
smaule1 <samuelzv18@gmail.com>
2025-05-01 18:43:13 -0600

    stat tripsByHourRange

--- src/controllers/statsController.js
+++ src/controllers/statsController.js
@@ -1,22 +1,60 @@
 const Trip = require("../models/Trip");
 const mongoose = require("mongoose");
 
+
+async function tripsByHour(startHour, endHour) {
+  
+  const trips = await Trip.aggregate([
+    { $project: { "hour": { $hour: "$departure" } } },
+    { $match: { "hour": { $gte: startHour, $lt: endHour } } },
+    { $count : "total"}]);
+  
+  return trips.length ? trips[0] : {total:0}
+   
+}
+
+// Retorna el total de viajes con timepo de salida entre startHour y endHour
 exports.getTripsByHour = async (req, res) => {
   try {
     const { startHour, endHour } = req.body
-
+    
     if(!startHour && startHour!=0  || !endHour && endHour!=0){
       return res.status(400).json({ msg: "Campos obligatorios faltantes." });
     }
 
-    let trips = await Trip.aggregate([
-      { $project: { "hour": { $hour: "$departure" } } },
-      { $match: { "hour": { $gte: startHour, $lte: endHour } } },
-      { $count : "total"}]);
-    
-    trips = trips.lenght || [{total:0}]
+    const trips = await tripsByHour(startHour, endHour);
 
     res.status(200).json({ msg: "Datos obtenidos exitosamente.", data: trips });
+  } catch (error) {
+    console.error("Error al obtener datos:", error);
+    res.status(500).json({ msg: "Error al obtener los datos." });
+  }
+};
+
+// Retorna una estadistica con los viajes agrupados por franja horaria.
+exports.getTripsByHourRange = async (req, res) => {
+  try {
+    let morningTrips, afternoonTrips, nightTrips = {};
+
+    const allTrips = await Trip.countDocuments();
+
+    morningTrips = await tripsByHour(6, 12);
+    morningTrips.percentage = morningTrips.total / allTrips * 100;
+    
+    afternoonTrips = await tripsByHour(12, 18);
+    afternoonTrips.percentage = afternoonTrips.total / allTrips * 100;
+    
+    nightTrips.total = allTrips - (morningTrips.total + afternoonTrips.total);
+    nightTrips.percentage = nightTrips.total / allTrips * 100;
+
+    const data = {
+      morningTrips: morningTrips,
+      afternoonTrips: afternoonTrips,
+      nightTrips: nightTrips,
+      total: allTrips
+    }
+
+    res.status(200).json({ msg: "Datos obtenidos exitosamente.", data: data });
   } catch (error) {
     console.error("Error al obtener datos:", error);
     res.status(500).json({ msg: "Error al obtener los datos." });
--- src/routes/stats.js
+++ src/routes/stats.js
@@ -1,5 +1,6 @@
 const statsController = require("../controllers/statsController");
 
 module.exports = function (app) {
-  app.get("/backend/stats/trip-hour", statsController.getTripsByHour);  
+  app.get("/backend/stats/trips-by-hour", statsController.getTripsByHour);
+  app.get("/backend/stats/trips-by-hour-range", statsController.getTripsByHourRange);  
 };
