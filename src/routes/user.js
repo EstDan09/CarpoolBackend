ba669a3c86022b3d61d07a82152e03f5dfc27ae8
Esteban Daniel Secaida Vargas <esecaidavargas@gmail.com>
2025-06-04 10:38:33 -0600

    post login

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,6 @@ const userController = require("../controllers/userController");
 
 module.exports = function (app) {
     app.post("/backend/user/register", userController.register);
-    app.get("/backend/user/login", userController.login);
+    app.post("/backend/user/login", userController.login);
     app.post("/backend/user/addCars", userController.addCars);
 }
