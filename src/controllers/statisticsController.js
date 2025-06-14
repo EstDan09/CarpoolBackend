8fe089e9b6c8f279cd919c15a83945a80fd50c5e
smaule1 <samuelzv18@gmail.com>
2025-06-14 17:24:02 -0600

    fix tripsByHourRange

--- src/controllers/statisticsController.js
+++ src/controllers/statisticsController.js
@@ -6,19 +6,19 @@ const mongoose = require("mongoose");
 exports.getTripsByHour = async (req, res) => {
   try {
     const startHour = req.query.start;
-    const endHour = req.query.end;    
+    const endHour = req.query.end;
 
-    if(!startHour && startHour!=0  || !endHour && endHour!=0){
+    if (!startHour && startHour != 0 || !endHour && endHour != 0) {
       return res.status(400).json({ msg: "Campos obligatorios faltantes." });
-    }    
-    
+    }
+
     const result = await Trip.aggregate([
       { $project: { "hour": { $hour: "$departure" } } },
-       { $match: { "hour": { $gte: Number(startHour), $lt: Number(endHour) } } },
-      { $count : "total"}]);
-        
-    const trips = result.length ? result[0] : {total:0}
-    
+      { $match: { "hour": { $gte: Number(startHour), $lt: Number(endHour) } } },
+      { $count: "total" }]);
+
+    const trips = result.length ? result[0] : { total: 0 }
+
     res.status(200).json({ msg: "Datos obtenidos exitosamente.", data: trips });
   } catch (error) {
     console.error("Error al obtener datos:", error);
@@ -28,29 +28,35 @@ exports.getTripsByHour = async (req, res) => {
 
 // Retorna una estadistica con los viajes agrupados por franjas horarias predefinidas.
 exports.getTripsByHourRange = async (req, res) => {
-  try {    
+  try {
 
     const result = await Trip.aggregate([
-      {$facet : {
-        total : [{$count: "total"}],
-        morning : [
-          { $project: { "hour": { $hour: "$departure" } } },
-          { $match: { "hour": { $gte: 6, $lt: 12 } } },
-          { $count : "total"}],
-        afternoon: [
-          { $project: { "hour": { $hour: "$departure" } } },
-          { $match: { "hour": { $gte: 12, $lt: 18 } } },
-          { $count : "total"}]
+      {
+        $facet: {
+          total: [{ $count: "total" }],
+          morning: [
+            { $project: { "hour": { $hour: "$departure" } } },
+            { $match: { "hour": { $gte: 6, $lt: 12 } } },
+            { $count: "total" }],
+          afternoon: [
+            { $project: { "hour": { $hour: "$departure" } } },
+            { $match: { "hour": { $gte: 12, $lt: 18 } } },
+            { $count: "total" }]
         }
       }
-    ]);    
+    ]);
 
-    const data  = {
-      total: result[0].total[0].total,
-      morning: result[0].morning[0].total,
-      afternoon: result[0].afternoon[0].total,
-      night: result[0].total[0].total - result[0].morning[0].total - result[0].afternoon[0].total
-    }  
+    let total = result[0].total?.[0]?.total ?? 0;
+    let morning = result[0].morning?.[0]?.total ?? 0;
+    let afternoon = result[0].afternoon?.[0]?.total ?? 0;
+    let night = total - morning - afternoon;
+
+    const data = {
+      total: total,
+      morning: morning,
+      afternoon: afternoon,
+      night: night,
+    };
 
     res.status(200).json({ msg: "Datos obtenidos exitosamente.", data: data });
   } catch (error) {
