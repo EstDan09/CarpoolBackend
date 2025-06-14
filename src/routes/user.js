7a21ea589c4d5569e4cfded36c22f1136e506369
smaule1 <samuelzv18@gmail.com>
2025-06-13 18:20:48 -0600

    id en params

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.post("/backend/user/register", userController.register);
     app.post("/backend/user/login", userController.login);
     app.post("/backend/user/addCars", userController.addCars);
-    app.put("/backend/user/update", userController.update); 
+    app.put("/backend/user/update:id", userController.update); 
     app.get("/backend/user/:id", userController.getUserById); 
     app.get("/backend/users", userController.getAllUsers); 
     app.post("/backend/user/add-notification", userController.addNotificationToUser);
