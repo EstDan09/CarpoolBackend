c17d702b2ac731e83e00f9f4a0b2736c46138225
Esteban Daniel Secaida Vargas <esecaidavargas@gmail.com>
2025-06-18 18:23:10 -0600

    ruta arreglada jeje

--- src/routes/trip.js
+++ src/routes/trip.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
   app.get("/backend/trips/user/:userId", tripController.getTripsByUser);
   app.put("/backend/trips/:id/cancel/:userId", tripController.cancelPassengerTrip);
   app.post("/backend/trip/filter", tripController.getTripsParams);
-  app.get("/backend/vehicle/getModel/:id", vehicleController.getModelVehicle);
+  app.get("/backend/vehicle/getModel/:id", tripController.getModelVehicle);
 };
 
 //GET /backend/trips/user/664abc123456?status=all
